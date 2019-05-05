98f962ef673fd88491d1a98c5226a8b363754349
newcoderlife <newcoderlife@outlook.com>
2019-05-05 14:07:39 +0800

    add station info window

--- src/components/SubwayMap.vue
+++ src/components/SubwayMap.vue
@@ -19,12 +19,25 @@ export default {
     });
     subway.addControl(zoomControl);
 
+    /*
     var startIcon = new BMapSub.Icon(
       "https://api.map.baidu.com/images/subway/start-bak.png",
       new BMapSub.Size(50, 80)
     );
     var marker = new BMapSub.Marker("近江", { icon: startIcon });
     subway.addMarker(marker);
+*/
+    var infowindow = new BMapSub.InfoWindow(
+      `
+      <div id="bd-subwayInfo">
+        <div id="bd-subwayTitle">
+          近江
+        </div>
+      </div>
+      `
+    );
+    subway.openInfoWindow(infowindow, "近江");
+    subway.setCenter("近江");
     subway.setZoom(1);
   }
 };
@@ -32,5 +45,10 @@ export default {
 <style>
 #subway-map {
   height: 100%;
+}
+
+#bd-subwayTitle {
+  text-align: center;
+  line-height: 50px;
 }
 </style>
