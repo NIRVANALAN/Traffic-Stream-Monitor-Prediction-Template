d137c858f235d05291857ee2ad9a4d86ba388c61
newcoderlife <newcoderlife@outlook.com>
2019-05-09 16:21:47 +0800

    seperate sidebar to component

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,6 @@
 <template>
   <el-container style="height: 100%; solid #eee; display: flex">
-    <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-      <el-menu :default-openeds="['1', '3']">
-        <el-submenu index="1">
-          <template slot="title">
-            <i class="el-icon-message"></i>数据统计
-          </template>
-          <el-menu-item-group>
-            <el-menu-item index="1-1">出站量</el-menu-item>
-            <el-menu-item index="1-2">换乘量</el-menu-item>
-            <el-menu-item index="1-3">客运量</el-menu-item>
-          </el-menu-item-group>
-        </el-submenu>
-        <el-submenu index="2">
-          <template slot="title">
-            <i class="el-icon-menu"></i>功能区域
-          </template>
-        </el-submenu>
-        <el-submenu index="3">
-          <template slot="title">
-            <i class="el-icon-setting"></i>我的收藏
-          </template>
-          <el-menu-item-group>
-            <template slot="title">路线</template>
-            <el-menu-item index="3-1">起点 - 终点</el-menu-item>
-            <el-menu-item index="3-2">起点 - 终点</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group>
-            <template slot="title">站点</template>
-            <el-menu-item index="3-3">站点</el-menu-item>
-            <el-menu-item index="3-4">站点</el-menu-item>
-          </el-menu-item-group>
-        </el-submenu>
-      </el-menu>
-    </el-aside>
+    <sidebar></sidebar>
 
     <el-container>
       <el-header style="text-align: right; font-size: 12px">
@@ -60,8 +27,13 @@
 </template>
 
 <script>
+import sidebar from "./components/Sidebar.vue";
+
 export default {
-  name: "app"
+  name: "app",
+  components: {
+    sidebar
+  }
 };
 </script>
 
@@ -70,12 +42,6 @@ export default {
   background-color: #b3c0d1;
   color: #333;
   line-height: 60px;
-}
-
-.el-aside {
-  color: #333;
-  position: relative;
-  height: 100%;
 }
 
 #map {
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,52 @@
+<template>
+  <el-aside>
+    <el-menu :default-openeds="['1', '3']">
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-message"></i>数据统计
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="1-1">出站量</el-menu-item>
+          <el-menu-item index="1-2">换乘量</el-menu-item>
+          <el-menu-item index="1-3">客运量</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+      <el-submenu index="2">
+        <template slot="title">
+          <i class="el-icon-menu"></i>功能区域
+        </template>
+      </el-submenu>
+      <el-submenu index="3">
+        <template slot="title">
+          <i class="el-icon-setting"></i>我的收藏
+        </template>
+        <el-menu-item-group>
+          <template slot="title">路线</template>
+          <el-menu-item index="3-1">起点 - 终点</el-menu-item>
+          <el-menu-item index="3-2">起点 - 终点</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group>
+          <template slot="title">站点</template>
+          <el-menu-item index="3-3">站点</el-menu-item>
+          <el-menu-item index="3-4">站点</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+    </el-menu>
+  </el-aside>
+</template>
+
+<script>
+export default {
+  name: "sidebar"
+};
+</script>
+
+<style>
+.el-aside {
+  color: #333;
+  position: relative;
+  height: 100%;
+  background-color: rgb(238, 241, 246);
+  width: "200px";
+}
+</style>
