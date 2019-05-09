37fd78483a4fe02484c6015c696d2bf450301a3e
newcoderlife <newcoderlife@outlook.com>
2019-05-09 16:34:19 +0800

    seperate header to component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,9 @@
 <template>
   <el-container style="height: 100%; solid #eee; display: flex">
-    <sidebar></sidebar>
+    <Sidebar></Sidebar>
 
     <el-container>
-      <el-header style="text-align: right; font-size: 12px">
-        <el-dropdown>
-          <i class="el-icon-setting" style="margin-right: 15px"></i>
-          <el-dropdown-menu slot="dropdown">
-            <el-dropdown-item>退出</el-dropdown-item>
-            <el-dropdown-item>详细</el-dropdown-item>
-            <el-dropdown-item>我的</el-dropdown-item>
-          </el-dropdown-menu>
-        </el-dropdown>
-        <span>王小虎</span>
-      </el-header>
-
+      <UserHeader></UserHeader>
       <el-main id="map">
         <iframe src="./map.html" frameborder="0" style="height: 100%; width: 100%"></iframe>
       </el-main>
@@ -27,23 +16,19 @@
 </template>
 
 <script>
-import sidebar from "./components/sidebar.vue";
+import UserHeader from "./components/UserHeader.vue";
+import Sidebar from "./components/Sidebar.vue";
 
 export default {
   name: "app",
   components: {
-    sidebar
+    Sidebar,
+    UserHeader
   }
 };
 </script>
 
 <style>
-.el-header {
-  background-color: #b3c0d1;
-  color: #333;
-  line-height: 60px;
-}
-
 #map {
   padding: 0%;
   position: relative;
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -37,7 +37,7 @@
 
 <script>
 export default {
-  name: "sidebar"
+  name: "Sidebar"
 };
 </script>
 
