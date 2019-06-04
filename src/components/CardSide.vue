36cb69eae7db3af194ee4faa92fca295f89ce50e
newcoderlife <newcoderlife@outlook.com>
2019-06-04 20:38:09 +0800

    remove warn card

--- src/components/CardSide.vue
+++ src/components/CardSide.vue
@@ -1,32 +1,26 @@
 <template>
   <el-aside id="card-side">
-    <WarnCard v-for="(warnContent, index) in warnCardContents" :key="index" :content="warnContent"></WarnCard>
     <InfoCard v-for="(infoCard, index) in infoCards" :infoCard="infoCard" :key="index"></InfoCard>
   </el-aside>
 </template>
 
 <script>
-import WarnCard from "./WarnCard.vue";
 import InfoCard from "./InfoCard.vue";
 import axios from "axios";
 
 export default {
   name: "CardSide",
-  components: { WarnCard, InfoCard },
+  components: { InfoCard },
   props: {},
   data() {
     return {
-      infoCards: [],
-      warnCardContents: []
+      infoCards: []
     };
   },
   mounted() {
     axios
       .get("//demo.mhtt1123.cn/infoCards")
       .then(response => (this.infoCards = response.data));
-    axios
-      .get("//demo.mhtt1123.cn/warnCardContent")
-      .then(response => (this.warnCardContent = response.data));
   }
 };
 </script>
